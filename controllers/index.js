c187e777166a1b04d428f7dad1c9d1a3a913fc3f
moniv <mohitv@exzeo.com>
2017-08-19 03:49:48 +0530

    adding filter for pr actions

--- controllers/index.js
+++ controllers/index.js
@@ -7,23 +7,24 @@ export default class Index {
 
     pullRequest = (req, res, next) => {
         const body = req.body;
-        const { number, pull_request, repository } = body;
+        const { action, number, pull_request, repository } = body;
         const { name, owner } = repository;
 
-        const reviewers = {
-            "reviewers": [
-                "moniv",
-                "sharma-sunil"
-            ],
-            "team_reviewers": []
-        };
+        if (['opened', 'closed', 'reopened', 'edited'].indexOf(action) !== -1) {
+            const reviewers = {
+                "reviewers": [
+                    "sharma-sunil"
+                ],
+                "team_reviewers": []
+            };
 
-        const gitService = new GitService(owner.login);
-        gitService.submitPullRequestReviewers(name, number, reviewers).then((response) => {
-            console.log(response);
-        }).catch((serverError) => {
-            console.log('some error while assigning review');
-        });
+            const gitService = new GitService(owner.login);
+            gitService.submitPullRequestReviewers(name, number, reviewers).then((response) => {
+                console.log(response);
+            }).catch((serverError) => {
+                console.log('Some error while assigning review');
+            });
+        }
 
         res.status(200).json(body);
     }
