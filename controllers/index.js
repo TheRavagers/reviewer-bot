d876b0a7cf30eaa32d9190547c7a4bd1a6a835f2
moniv <mohitv@exzeo.com>
2017-08-19 01:42:04 +0530

    making it compatible with heroku

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ export default class Index {
     pullRequest = (req, res, next) => {
         const body = req.body;
 
-        res.status(200).json({});
+        res.status(200).json(body);
 
     }
 
