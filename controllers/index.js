f9336312da7841b4eed171c2cedfecb91ab54e3b
moniv <mohitv@exzeo.com>
2017-08-18 22:54:27 +0530

    adding arrow fun support

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ export default class Index {
 
     constructor() { }
 
-    pullRequest(req, res, next) {
+    pullRequest = (req, res, next) => {
         const body = req.body;
 
         res.status(200).json({});
